8fabe56d050d4228a346d35d73ec71f1a30b400c
Werner Banyacskay <werner.x64@gmail.com>
2017-05-22 20:32:35 +0300

    Added views

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+function get_base_url() {
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+        $base_url = "https://" . $_SERVER['HTTP_HOST'];
+    } else {
+        $base_url = "http://" . $_SERVER['HTTP_HOST'];
+    }
+
+    $base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+
+    return $base_url;
+}
+
+Flight::set('flight.views.path', 'app/views');
+
 Flight::route('/', function(){
-  echo 'hello world!';
+  Flight::render('home.php', []);
 });
 
 Flight::start();
