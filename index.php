7cd76b8c2eb9db30c0bc6ad24ef6d6d7397570f8
Werner Banyacskay <werner.x64@gmail.com>
2017-05-24 08:35:52 +0300

    Added train and predict

--- index.php
+++ index.php
@@ -86,4 +86,48 @@ Flight::route('/logout', function() {
 	header('Location: ' . filter_var(getBaseUrl(), FILTER_SANITIZE_URL));
 });
 
+Flight::route('/train', function() {
+	if (!$oauth_credentials = getOAuthCredentialsFile()) {
+	  echo 'no file';
+	  return;
+	}
+
+  	$client = new Google_Client();
+	$client->setAuthConfig($oauth_credentials);
+	$client->setRedirectUri(getBaseUrl());
+	$client->addScope("https://www.googleapis.com/auth/prediction");
+	$client->addScope("https://www.googleapis.com/auth/devstorage.full_control");
+	$client->addScope("https://www.googleapis.com/auth/devstorage.read_write");
+
+	$insert = new Google_Service_Prediction_Insert($client);
+
+	  $insert->setId('velvety-height-156418');
+	  $insert->setStorageDataLocation('training_data_set/training_data_set.csv'); // A file in Cloud Storage, must be upload first
+	  $result = $service->trainedmodels->insert($project, $insert);
+});
+
+Flight::route('/predict', function() {
+	if (!$oauth_credentials = getOAuthCredentialsFile()) {
+	  echo 'no file';
+	  return;
+	}
+
+  	$client = new Google_Client();
+	$client->setAuthConfig($oauth_credentials);
+	$client->setRedirectUri(getBaseUrl());
+	$client->addScope("https://www.googleapis.com/auth/prediction");
+	$client->addScope("https://www.googleapis.com/auth/devstorage.full_control");
+	$client->addScope("https://www.googleapis.com/auth/devstorage.read_write");
+
+	$service = new Google_Service_Prediction($client);
+
+	$predictionText = 'free 4/10/2017 4:02:10, gb, 4/12/2017, 6, 0, en, 123, 0, 5, website, website, organic, -, -, free, /, 29, 1, 0, 0, 0, 0, 0, 0, 0, 0';
+	$predictionData = new Google_Service_Prediction_InputInput();
+	$predictionData->setCsvInstance(array($predictionText));
+
+	$input = new Google_Service_Prediction_Input();
+	$input->setInput($predictionData);
+	$hostedmodels = $service->trainedmodels->predict($project, 'velvety-height-156418', $input);
+});
+
 Flight::start();
